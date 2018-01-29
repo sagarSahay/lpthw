6fb9e997c8f71ecb3a4610383cd479a440f01d5f
sagarSahay <sagar.sahay@gmail.com>
2018-01-29 06:41:34 +0000

    two sums

---
+++ twoSum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def twoSum(self, nums, target):
+        nums_len = len(nums)
+        for i in range(0,nums_len):
+            for j in range(i+1, nums_len-1):
+                if nums[i] + nums[j] == target:
+                    result = []
+                    result.append(nums[i])
+                    result.append(nums[j])
+                    return result
+
+
+
+def main():
+    import sys
+
+    while True:
+        try:
+
+            ret = Solution().twoSum([1,3,4,5], 9)
+
+            
+            print(ret)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
